bf260890d42a2cb68bef62b90e8d66f23e11cc29
seongmoon729 <jsm21star@gmail.com>
2023-04-16 02:24:08 +0900

    fix bug (`~/linuxbrew` -> `~/.linuxbrew`)

--- setup.sh
+++ setup.sh
@@ -12,16 +12,18 @@ ln -sf "$(pwd)/.config/fish" "${HOME}/.config/fish"
 ln -sf "$(pwd)/.config/tmux/tmux.conf" "${HOME}/.tmux.conf"
 
 # Install homebrew.
-os_name="$(uname -s)"
-if [[ $os_name == "Linux" ]]; then
-    mkdir "${HOME}/linuxbrew"
-    curl -L "https://github.com/Homebrew/brew/tarball/master" | tar xz --strip 1 -C "${HOME}/linuxbrew"
-    eval "$(${HOME}/.linuxbrew/bin/brew shellenv)"
-elif [[ $os_name == "Darwin" ]]; then
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+os_kernel_name="$(uname -s)"
+if [[ $os_kernel_name == "Linux" ]]; then
+    linux_brew_path="${HOME}/.linuxbrew"
+    mkdir "${linux_brew_path}"
+    curl -L "https://github.com/Homebrew/brew/tarball/master" | \
+        tar xz --strip 1 -C "${linux_brew_path}"
+    eval "$(${linux_brew_path}/bin/brew shellenv)"
+elif [[ $os_kernel_name == "Darwin" ]]; then
+    bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     eval $(/opt/homebrew/bin/brew shellenv)
 else
-    echo "Not supported OS" $os_name
+    echo "Not supported OS" $os_kernel_name
 fi
 brew developer off
 
@@ -44,19 +46,20 @@ brew install gh
 brew install tmux
 
 # Change default shell to fish and add `brew` path.
-if [[ $os_name == "Linux" ]]; then
+if [[ $os_kernel_name == "Linux" ]]; then
     echo "exec $(which fish) -l" > "${HOME}/.bash_profile" 
-    fish -c "fish_add_path ${HOME}/.linuxbrew/bin"
-elif [[ $os_name == "Darwin" ]]; then
+    fish -c "fish_add_path ${linux_brew_path}/bin"
+elif [[ $os_kernel_name == "Darwin" ]]; then
     echo "$(which fish)" | sudo tee -a "/etc/shells"
     sudo chsh -s "$(which fish)"  
     fish -c "fish_add_path /opt/homebrew/bin"
 else
-    echo "Not supported OS" $os_name
+    echo "Not supported OS" $os_kernel_name
 fi
 
 # Install fisher (plugin manger for fish shell).
-fish -c "curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher"
+fish -c "curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | \
+    source && fisher install jorgebucaran/fisher"
 fish -c "fisher install jethrokuan/z"  # install `z`.
 
 # Kill tmux server if older version is already running on the machine.
