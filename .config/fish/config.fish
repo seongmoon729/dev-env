37d9f13cf57cc31bd382f8223d1e37b9f3e51020
seongmoon729 <jsm21star@gmail.com>
2023-04-16 18:35:36 +0900

    add function `st` that imply ssh-tmux

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,7 @@
 # Replace `ll` with `exa`.
 if type -q exa
-  alias ll "exa -l -g --icons"
-  alias lla "ll -a"
+    alias ll "exa -l -g --icons"
+    alias lla "ll -a"
 end
 
 # Neovim.
@@ -15,3 +15,8 @@ starship init fish | source
 
 # Set vi key binding mode.
 fish_vi_key_bindings
+
+# SSH command with tmux argument.
+function st
+    ssh -t $argv -- /bin/sh -c 'tmux has-session && exec tmux attach || exec tmux'
+end
