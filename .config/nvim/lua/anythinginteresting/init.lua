548dcb031beb7a1a1af3582fffc06c96d7a1c3e0
seongmoon729 <jsm21star@gmail.com>
2023-04-16 22:27:18 +0900

    hotfix for bootstrapping

--- .config/nvim/lua/anythinginteresting/init.lua
+++ .config/nvim/lua/anythinginteresting/init.lua
@@ -1,14 +1,3 @@
--- Install packer.nvim if not exist.
-local packerpath = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
-if not vim.loop.fs_stat(packerpath) then
-    vim.fn.system {
-        'git',
-	'clone',
-	'--depth=1',
-	'https://github.com/wbthomason/packer.nvim',
-	packerpath,
-    }
-end
-
+require('anythinginteresting.packer')
 require('anythinginteresting.remap')
 require('anythinginteresting.set')
--- .config/nvim/lua/anythinginteresting/packer.lua
+++ .config/nvim/lua/anythinginteresting/packer.lua
@@ -1,6 +1,19 @@
-vim.cmd.packadd('packer.nvim')
+-- Install packer.
+local packerpath = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
+local is_bootstrap = false
+if not vim.loop.fs_stat(packerpath) then
+    vim.fn.system {
+        'git',
+	    'clone',
+	    '--depth=1',
+	    'https://github.com/wbthomason/packer.nvim',
+	    packerpath,
+    }
+    is_bootstrap = true
+    vim.cmd.packadd('packer.nvim')
+end
 
-return require('packer').startup(function(use)
+require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
 
   use {
@@ -40,5 +53,26 @@ return require('packer').startup(function(use)
       {'L3MON4D3/LuaSnip'},     -- Required
     }
   }
+  if is_bootstrap then
+      require('packer').sync()
+  end
 
 end)
+
+if is_bootstrap then
+  print '=================================='
+  print '    Plugins are being installed'
+  print '    Wait until Packer completes,'
+  print '       then restart nvim'
+  print '=================================='
+  return
+end
+
+-- Automatically source and re-compile packer whenever you save this init.lua
+local packer_group = vim.api.nvim_create_augroup('Packer', { clear = true })
+vim.api.nvim_create_autocmd('BufWritePost', {
+  command = 'source <afile> | PackerCompile',
+  group = packer_group,
+  pattern = vim.fn.expand '$MYVIMRC',
+})
+
